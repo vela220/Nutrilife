1fe044f1eb6d597f41b072498296b99b3e830979
Kenia <23308060610371@cetis61.edu.mx>
2025-11-03 10:07:01 -0500

    x

--- app.py
+++ app.py
@@ -12,22 +12,25 @@ def inicio():
 def registro():
     return render_template("formulario.html")
 
+@app.route("/sesion")
+def sesion():
+    return render_template("sesion.html")
+
 @app.route("/dieta")
 def dieta():
     return render_template("dieta.html")
 
-@app.route("/horario")
+@app.route("/horarioC")
 def horario():
-    return render_template("horario.html")
+    return render_template("horarioC.html")
 
 @app.route("/recetas")
-def recetas():
+def recatas():
     return render_template("recetas.html")
 
 @app.route("/acerca")
 def acerca():
     return render_template("acerca.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
